77d5a970476f4f353d4582e5d93964cacd30cd0e
Hisham <hisham0502@gmail.com>
2022-11-27 19:31:54 +0530

    Java hangman project

---
+++ Hangman.java
@@ -0,0 +1,110 @@
+import java.lang.*;
+import java.util.Scanner;
+// import java.util.Arrays;
+
+public class Hangman {
+
+    public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+    "wombat", "zebra"};
+
+    public static String[] gallows = {"+---+\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|   |\n" +
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" +
+    "/    |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + 
+    "/ \\  |\n" +
+    "     |\n" +
+    " =========\n"};
+
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+
+        int ind=(int)(Math.floor(Math.random()*(words.length)));
+        String word=words[ind];
+        System.out.println("Welcome to Java Hangman!\n");
+        String ans= "_ ".repeat(word.length());
+        char prev=' ';
+        int count=0;
+        int chances=gallows.length;
+
+        while(count < chances){
+        char guess=prev;
+        System.out.println("Guess: " + guess);
+        System.out.println(gallows[count]);
+        System.out.println("Word: " + ans);
+        System.out.println("Misses: ");
+        System.out.print("Guess: ");
+        prev=scan.next().charAt(0);
+        System.out.println("\n\n");
+        if(word.contains(Character.toString(prev))){
+            int charInd=word.indexOf(Character.toString(prev));
+        } else{
+            count++;
+        } 
+        
+    }
+    
+    scan.close();
+}
+
+}
+
+
+
+
+
